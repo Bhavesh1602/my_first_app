0bbf4968f549225e8771c96744f0135f108dcdd4
Bhavesh Vadlakonda <113020882+Bhavesh1602@users.noreply.github.com>
2023-02-28 21:47:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello Everyone:smile: I am :blue[Bhavesh] and here is my first Web Applicaton")
+
+
+btn_click = st.button("Know More!")
+
+if btn_click == True:
+    st.subheader("linkedIn: www.linkedin.com/in/bhavesh-vadlakonda-628a56242")
